37bf8cfacbd7ec63ef2177a1e8a864cfdca702a0
MichiganLaguna <michiganlaguna@gmail.com>
2022-06-13 20:55:01 +0200

    Messing around with the parser

--- __main__.py
+++ __main__.py
@@ -1,28 +1,35 @@
 """Discord bot for the server 42."""
+import os
 import logging
-from configparser import ConfigParser
+from configparser import ConfigParser, ExtendedInterpolation
 from time import time
 import aiohttp
 import discord
 
-CONFIG = ConfigParser()
-FILE_NAME = "config.cfg"
-CONFIG.read(FILE_NAME)
+CURR_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-logger = logging.getLogger("discord")
-# %Y-%m-%d_%H-%M-%S_%Z
-logger.setLevel(logging.DEBUG)
+CONFIG = ConfigParser(interpolation=ExtendedInterpolation())
+CONFIG_FILE = os.path.join(CURR_FOLDER, 'config.cfg')
+CONFIG.read(CONFIG_FILE)
+
+VARIABLE = ConfigParser(interpolation=ExtendedInterpolation())
+VARIABLE_FILE = os.path.join(CURR_FOLDER, CONFIG.get("Language", "default_language"))
+VARIABLE.read(VARIABLE_FILE)
+
+os.makedirs(CONFIG.get("Logging", "log_dir"), exist_ok=True)
+logger = logging.getLogger(CONFIG.get("Logging", "logger"))
+# %Y-%m-%d_%H-%M-%S_%Z : old time format for log files
+logger.setLevel(CONFIG.getint("Logging", "logger_level"))
 handler = logging.FileHandler(
-    filename=f"log/{time()}.log",
+    filename=f"logs/{time()}.log",
     encoding="utf-8",
     mode="w",
 )
 handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+    logging.Formatter(CONFIG.get("Logging", "handler_format"))
 )
 logger.addHandler(handler)
 
-LOG_CHANNEL = CONFIG.getint("Channel", "zbi")
 TOKEN = CONFIG.get("Bot", "token")
 PREFIX = CONFIG.get("Bot", "prefix")
 
@@ -34,31 +41,48 @@ class MyClient(discord.Client):
     async def on_ready(self):
         """Prints when the bot is ready."""
         print(f"Logged in as {self.user} (ID: {self.user.id})")
+        
+    async def create_invite(self, guild):
+        """Creates an invite for the given channel."""
+        for channel in guild.channels:
+            if isinstance(channel, discord.TextChannel):
+                return await channel.create_invite(max_age=0, max_uses=0)
+            
 
     async def on_message(self, message):
         """Prints when a message is sent."""
         if message.author == self.user:
             return
 
-        if message.content.startswith(f"{PREFIX}hello"):
-            await message.channel.send(f"{PREFIX}hello")
+        if message.content.startswith(f"{PREFIX}{VARIABLE.get('OnMessageQ', 'hello')}"):
+            await message.channel.send(f"{PREFIX}{VARIABLE.get('OnMessageA', 'hello')}")
 
-        if message.content.startswith(f"{PREFIX}ping"):
-            await message.author.send("Pong!")
-
+        if message.content.startswith(f"{PREFIX}{VARIABLE.get('OnMessageQ', 'pasta')}") or message.content.startswith(f"{PREFIX}{VARIABLE.get('OnMessageQ', 'pasta_1')}"):
+            img = f"{CONFIG.get('Ressources', 'pasta_image_name')}"
+            file = discord.File(CONFIG.get("Ressources", "pasta_image"), filename=img)
+            embed = discord.Embed(color=0x00ff00)
+            embed.set_image(url=f"attachment://{img}")
+            await message.author.send(embed=embed, file=file)
+            
+        if message.content.startswith(f"{PREFIX}{VARIABLE.get('OnMessageQ', 'whereareyou')}"):
+            await message.channel.send(f"{VARIABLE.get('OnMessageA', 'whereareyou')}")
+            for guild in self.guilds:
+                invite = await self.create_invite(guild) # Creates an invite for the guild
+                await message.channel.send(invite.url) # Sends the invite to the channel
+            
         if message.content.startswith(f"{PREFIX}cat"):
             async with aiohttp.ClientSession() as session:
                 async with session.get("https://aws.random.cat/meow") as resp:
                     if resp.status != 200:
-                        answer = resp.status + resp.reason
+                        answer = f"{resp.status}: {resp.reason}"
                         return await message.channel.send(f"{answer}")
                     data = await resp.json()
                     await message.channel.send(data["file"])
 
-
 activity = discord.Activity(
     name="How to make good spaghetti",
-    type=discord.ActivityType.watching,
+    type=discord.ActivityType.competing,
 )
 client = MyClient(activity=activity, intents=discord.Intents.default())
+
 client.run(TOKEN)
